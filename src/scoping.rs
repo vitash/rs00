4315f6cd3a0ac0a015368ca1ed8227c446876755
vitash <18176159774@163.com>
2020-04-30 18:41:20 +0800

    match @

--- src/scoping.rs
+++ src/scoping.rs
@@ -8,6 +8,18 @@ pub fn test() {}
 fn fn1() {
     let p1 = Point { x: 3, y: 4 };
     let Point { ref x, .. } = p1;
-    let b = x;
-    println!("{}", b == &3);
+}
+
+fn fn2(a: i32, b: Option<i32>) {
+    match a {
+        ten @ 0..=10 => println!("{}", ten),
+        other => println!("{}", other),
+    }
+    match b {
+        Some(b @ 0..=20) => b,
+        Some(b) if (0..2).contains(&b) => b,
+        _ => 30,
+    };
+    // if let Some(c @ 0..3) != b { return; } // 语法和后面的语义不支持，格式化也不支持三行变成一行
+    // println!("{}, c in 1..3", c);
 }
